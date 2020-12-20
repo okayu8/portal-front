d3800dba1b2b1fdd8a1c257d9de4eab37f8212d7
片岡祐太郎 <hitugaya12@gmail.com>
2020-12-21 02:10:19 +0900

    add components, edit package.json

--- src/login.tsx
+++ src/login.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-ReactDOM.render(<h1>hello login page.</h1>, document.querySelector('#root'))
+import Login from './components/Login'
+
+ReactDOM.render(<Login />, document.querySelector('#root'))
