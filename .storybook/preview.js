18a2e757e7b5375edad54b47c70bff7d8c7586b8
片岡祐太郎 <hitugaya12@gmail.com>
2020-12-30 01:32:41 +0900

    add global styles

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,28 @@
+import { createGlobalStyle } from 'styled-components'
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
 }
+
+export const decorators = [
+  (Story) => (
+    <>
+      <GlobalStyle />
+      <Story />
+    </>
+  ),
+]
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    font-family: "-apple-system","BlinkMacSystemFont","Helvetica Neue","Segoe UI","Hiragino Kaku Gothic ProN","Hiragino Sans","ヒラギノ角ゴ ProN W3","Arial","メイリオ","Meiryo","sans-serif";
+    line-height: 1.75;
+    font-weight: 400;
+    font-size: 16px;
+    -webkit-letter-spacing: .03em;
+    -moz-letter-spacing: .03em;
+    -ms-letter-spacing: .03em;
+    letter-spacing: .03em;
+    color: rgba(0,0,0,0.87);
+  }
+`
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import { createGlobalStyle } from 'styled-components'
+
+import { Color } from '../const/color'
 
 const App: React.FC = () => {
-  return <>Hello App</>
+  return (
+    <>
+      <GlobalStyle />
+      <div>Hello App</div>
+    </>
+  )
 }
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    font-family: "-apple-system","BlinkMacSystemFont","Helvetica Neue","Segoe UI","Hiragino Kaku Gothic ProN","Hiragino Sans","ヒラギノ角ゴ ProN W3","Arial","メイリオ","Meiryo","sans-serif";
+    line-height: 1.75;
+    font-weight: 400;
+    font-size: 16px;
+    -webkit-letter-spacing: .03em;
+    -moz-letter-spacing: .03em;
+    -ms-letter-spacing: .03em;
+    letter-spacing: .03em;
+    color: ${Color.cover_black};
+  }
+`
 
 export default App
